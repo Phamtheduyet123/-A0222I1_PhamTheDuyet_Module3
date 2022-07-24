25f7837bbd1a5531f15ecdb1cc61450e937f891a
Phamtheduyet123 <duyetpham322@gmail.com>
2022-07-24 22:31:10 +0700

    nop bai

---
+++ thiet_ke_va_tao_csdl/bai_tap_quan_li_ban_Hang.sql
@@ -0,0 +1,28 @@
+create database if not exists quan_ly_ban_hang;
+use quan_ly_ban_hang;
+create table customer(
+cID int auto_increment primary key,
+cName varchar(50) not null,
+cAge int not null check(cAge >=18)
+);
+create table `order`(
+oID int auto_increment primary key,
+cID int not null ,
+oDate date not null,
+oTotalPrice double not null,
+foreign key (cID) references customer(cID)
+);
+create table product(
+pID int auto_increment,
+pName varchar(20) not null,
+pPrice double not null ,
+primary key(pID)
+);
+create table orderdetail(
+oID_order int ,
+pID_product int ,
+odQTY varchar(20) not null,
+primary key(oID_order,pID_product),
+foreign key (oID_order) references `order`(oID),
+foreign key (pID_product) references product(pID)
+);
